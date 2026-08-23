8b7c2e9d750c168e06fdb8fdb53eda18445f338d
lithishhhh <lithishv141@gmail.com>
2026-08-23 13:00:55 +0530

    Updated backend shii

--- backend/index.js
+++ backend/index.js
@@ -10,16 +10,28 @@ app.get("/", (req, res) => {
     res.send("FilmFeast backend is running!");
 });
 
+
 app.post("/api/login", (req, res) => {
+
     console.log("LOGIN REQUEST RECEIVED");
     console.log(req.body);
 
     const { email, password } = req.body;
 
     if (email === "user@gmail.com" && password === "0011") {
-        res.send("Login successful");
+
+        res.status(200).send({
+            success: true,
+            message: "Login successful"
+        });
+
     } else {
-        res.send("Invalid email or password");
+
+        res.status(401).send({
+            success: false,
+            message: "Invalid email or password"
+        });
+
     }
 });
 
