20cd43a2b2e63e0a973ec271294b3d78f37b855f
lithishhhh <lithishv141@gmail.com>
2026-08-23 10:43:21 +0530

    Fix backend cors

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,12 @@ const cors = require("cors");
 
 const app = express();
 
-
 app.use(cors({
     origin: "https://filmfeast-react-f48c-5tm9co0ik-lithishhhhs-projects.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"]
 }));
+
 app.use(express.json());
 
 const user = {
@@ -21,20 +21,18 @@ app.post("/api/login", (req, res) => {
     const { email, password } = req.body;
 
     if (email === user.email && password === user.password) {
-        res.send({
+        return res.send({
             message: "Login successful",
             success: true
         });
-    } else {
-        res.send({
-            message: "Invalid email or password",
-            success: false
-        });
     }
+
+    res.status(401).send({
+        message: "Invalid email or password",
+        success: false
+    });
 });
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Server started");
 });
